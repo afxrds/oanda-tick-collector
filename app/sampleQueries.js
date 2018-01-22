ccb576568d66b93bb8e645504c6a1487c6d63e33
Marcin Jedyk <m.jedyk@gmail.com>
2018-01-22 20:35:30 +0000

    adjust sample

--- app/sampleQueries.js
+++ app/sampleQueries.js
@@ -1,6 +1,9 @@
-db.candles.find({ 'tick.time': { '$gte': ISODate('2016-08-12T19:35:00.000Z'), '$lt': ISODate('2015-12-17T16:00:02.000Z') }, 'tick.instrument': 'EUR_USD' })
+db = db.getSiblingDB("instruments");
 
-db.candles.find({ 'tick.time': { '$gte': ISODate('2016-08-12T19:35:00.000Z')}, 'tick.instrument': 'GBP_USD' })
+
+db.ticks.find({ 'tick.time': { '$gte': ISODate('2016-08-12T19:35:00.000Z'), '$lt': ISODate('2015-12-17T16:00:02.000Z') }, 'tick.instrument': 'EUR_USD' })
+
+db.ticks.find({ 'tick.time': { '$gte': ISODate('2016-08-12T19:35:00.000Z')}, 'tick.instrument': 'GBP_USD' })
 
 
 db.ticks.count({ 'tick.time': { '$gte': ISODate('2016-06-01T19:35:00.000Z')}, 'tick.instrument': 'EUR_USD' })
